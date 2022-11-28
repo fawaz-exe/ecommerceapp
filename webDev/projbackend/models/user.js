0b31dcb95cd2f7c6c9212f143010b25415624055
fawaz-exe <fawaz.exe@gmail.com>
2022-11-28 16:17:48 +0530

    Backend Model - Category Done

--- webDev/projbackend/models/user.js
+++ webDev/projbackend/models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const crypto = require("crypto");
+const uuidv1 = require('uuid/v1');
 
 import mongoose from 'mongoose';
 const { Schema } = mongoose;
@@ -45,8 +47,50 @@ const userSchema = new Schema({
     purchases: {
         type: Array,
         default: []
+    },
+
+    encry_password: {
+        type: String,
+        required: true
+
     }
 
-});
+}, {timestamps : true});
+
+
+    //Virtual Fields
+userSchema.virtual("password")
+    .set(function(password) {
+        this._password = password
+        this.salt =  uuidv1();
+        this.encry_password = this.securePassword(password);
+
+    })
+    .get(function(){
+        return this._password;
+    })
+
+
+
+userSchema.method = {
+
+    authenticate: function(plainpassword){
+        return this.securePassword(plainpassword === this.encry_password);
+    },
+
+    securePassword: function(plainpassword){
+        if(!password)
+        return "";
+
+        try {
+            return crypto
+            .createHmac('sha256', secret)
+            .update(plainpassword)
+            .digest('hex'); 
+        } catch (error) {
+            return ""; 
+        }
+    }
+}
 
 module.exports = mongoose.model("User", userSchema);
